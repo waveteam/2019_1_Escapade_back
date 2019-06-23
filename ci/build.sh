8e8e6a24cdc574ec7f4a2e4c15abf8878ef40dc3
bomkvilt <kvilt-d.max@yandex.ru>
2019-06-23 16:24:31 +0300

    [wave] fucken docker-compose..

--- ci/build.sh
+++ ci/build.sh
@@ -9,30 +9,7 @@ call()
     "$@" && return || echo " -- command: '$@' failed" && exit 1
 }
 
-push()
-{   # tag the images and push to the docker hub
-    NAME="$1"
-    IMAGE="${PROJECT}_${NAME}"
-    BUILD="${REPO}/${PROJECT}-${NAME}:latest"
-    echo " >> assigning an image '${IMAGE}' to '${BUILD}'..."
-    call docker tag ${IMAGE} ${BUILD}
-    echo " >> pushing an image '${BUILD}'..."
-    call docker push ${BUILD}
-    echo " >> image '${BUILD}' was processed!"
-}
-
 # invoke docker-compose build; -> ${PROJECT}_*
 echo " >> starting docker-compose build..."
 call sudo docker-compose -p $PROJECT --skip-hostname-check build --parallel
 echo " >> docker-compose build was completed!"
-
-# push the images asyncronously
-for NAME in         \
-    auth            \
-    api             \
-    game            \
-    history         \
-    prometheus      \
-; do 
-    push ${NAME} &
-done
---
+++ ci/push.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# enviroment
+PROJECT='escapade'
+REPO='wavepark'
+
+call() 
+{   # invoke the command sucessfuly or terminate
+    "$@" && return || echo " -- command: '$@' failed" && exit 1
+}
+
+push()
+{   # tag the images and push to the docker hub
+    NAME="$1"
+    IMAGE="${PROJECT}_${NAME}"
+    BUILD="${REPO}/${PROJECT}-${NAME}:latest"
+    echo " >> assigning an image '${IMAGE}' to '${BUILD}'..."
+    call docker tag ${IMAGE} ${BUILD}
+    echo " >> pushing an image '${BUILD}'..."
+    call docker push ${BUILD}
+    echo " >> image '${BUILD}' was processed!"
+}
+
+# push the images asyncronously
+for NAME in         \
+    auth            \
+    api             \
+    game            \
+    history         \
+    prometheus      \
+; do 
+    push ${NAME} &
+done

f6841f76ef7ff6f9323f49399b5405398cd2d5e5
bomkvilt <kvilt-d.max@yandex.ru>
2019-06-23 16:44:41 +0300

    [wave] non parallel build

--- ci/build.sh
+++ ci/build.sh
@@ -11,5 +11,5 @@ call()
 
 # invoke docker-compose build; -> ${PROJECT}_*
 echo " >> starting docker-compose build..."
-call sudo docker-compose -p $PROJECT --skip-hostname-check build --parallel
+call sudo docker-compose -p $PROJECT --skip-hostname-check build #--parallel
 echo " >> docker-compose build was completed!"
